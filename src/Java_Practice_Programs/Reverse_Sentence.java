4786f8457ca3f033c19537d431791a1ace0dbec1
shalini4007@gmail.com <shalini.v06@ad.infosys.com>
2025-02-03 18:59:05 +0530

    code changes

--- src/Java_Practice_Programs/Reverse_Sentence.java
+++ src/Java_Practice_Programs/Reverse_Sentence.java
@@ -5,13 +5,8 @@ import java.util.Arrays;
 public class Reverse_Sentence {
 
 	public static void main(String[] args) {
-		String sen = "This is Java Class";
-		String rev_sen = "";
-		for (int i = sen.length() - 1; i >= 0; i--) {
-			rev_sen = rev_sen + sen.charAt(i);
-		}
-		System.out.println("Reversed Sentence is: " + rev_sen);
 		
+	String sen= " this is pen";
 		String arr[]=sen.split(" ");
 		System.out.println(Arrays.toString(arr));
 		for(int i=0;i<arr.length; i++)

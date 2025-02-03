3133a82b1811b2bf684acec5d0e7659a8cad3758
shalini4007@gmail.com <shalini.v06@ad.infosys.com>
2025-02-03 19:01:46 +0530

    code changes

--- src/Java_Practice_Programs/Remove_Duplicates_From_A_String.java
+++ src/Java_Practice_Programs/Remove_Duplicates_From_A_String.java
@@ -3,16 +3,7 @@ package java_Practice_Programs;
 public class Remove_Duplicates_From_A_String {
 
 	public static void main(String[] args) {
-		String a="Shalini";
-	      for(int i=0; i<=a.length()-1; i++)
-	      {
-	         char flag=(i);
-	          for(int j=i+1; j<=a.length()-1; j++)
-	          {
-	             if(a(i).equalsToa(j));
-	             a.remove(i);
-	          }
-	      }
+		
 
 	}
 
